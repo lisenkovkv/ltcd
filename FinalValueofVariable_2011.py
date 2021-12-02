f148910c735891d4f260e2b6fee0f2965fe06f34
LisenkovKV <lisenkov_kirill@mail.ru>
2021-12-02 15:14:06 +0300

    hello

--- FinalValueofVariable_2011.py
+++ FinalValueofVariable_2011.py
@@ -5,10 +5,15 @@ def finalValueAfterOperations(operations):
         :type operations: List[str]
         :rtype: int
         """
-        x = 0
-        for op in operations:
-            if op.find('-') > -1:
-                x-=1
-            else:
-                x+=1
-        return x
+        #x = 0
+        #for op in operations:
+        #    if op.find('-') > -1:
+        #        x-=1
+        #    else:
+        #        x+=1
+        #return x
+# Runtime: 40 ms, faster than 47.69% of Python online submissions for Final Value of Variable After Performing Operations.
+# Memory Usage: 13.7 MB, less than 13.12% of Python online submissions for Final Value of Variable After Performing Operations.
+        return len(operations) - str(operations).count('-')
+#Runtime: 40 ms, faster than 47.69% of Python online submissions for Final Value of Variable After Performing Operations.
+#Memory Usage: 13.5 MB, less than 67.64% of Python online submissions for Final Value of Variable After Performing Operations.
